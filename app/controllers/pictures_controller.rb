745cfeaa049388991bd2710cdf9094f52c34e1c0
Cody Condon <codycondon50@gmail.com>
2016-01-25 15:12:06 -0500

    Adding feature to edit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,7 +3,22 @@ class PicturesController < ApplicationController
   def index
   @pictures = Picture.all
   end
-  
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+    def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+
   def new
     @picture = Picture.new
   end
